6e4efd29a0b496cdd1f488a31873d9b5f79d90f2
Haley <arizonagranger@gmail.com>
2021-03-24 01:04:58 -0400

    updated, out of range error

--- AI/randomAI.py
+++ AI/randomAI.py
@@ -1,19 +1,25 @@
 import random
+import requests
+URL = "http://localhost:4850/"
+
 def random_move(team):
     boardCord = ["a", "b", "c", "d", "e", "f", "g", "h"]
     delegates = ["L", "R", "K"]
     moves = []
     while len(delegates) > 0:
         del_moves = []
-        del_pieces = request.post(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random.randrange(len(delegates)))).text.split(",")
+        del_pieces = requests.post(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random.randrange(len(delegates)))).text.split(",")
         for piece in del_pieces:
-            del_moves.append(request.post(url = URL + "actions-for?coord=" + boardCord[piece[0]]+str(piece[1]+1)).text.split("\n"))
+            del_moves.append(requests.post(url = URL + "actions-for?coord=" + boardCord[piece[0]]+str(piece[1]+1)).text.split("\n"))
         random_move = del_moves[random.randrange(len(del_moves))]
-        submitMove(request.post(url = URL + "actions-for?coord=" + move)) ##this would be where the ai would send to the move to rules and does everything with the board state
+        submitMove(requests.post(url = URL + "actions-for?coord=" + move)) ##this would be where the ai would send to the move to rules and does everything with the board state
         moves.append(random_move)
     return moves
 
 
+
+
+print(random_move("Black"))
 
 
 
