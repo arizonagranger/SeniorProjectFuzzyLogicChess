1a24f43391e0da63040c4276e3ff3e21925b424d
gdepew11 <61752838+gdepew11@users.noreply.github.com>
2021-03-24 00:52:49 -0400

    Update randomAI.py

--- AI/randomAI.py
+++ AI/randomAI.py
@@ -1,13 +1,14 @@
+import random
 def random_move(team):
     boardCord = ["a", "b", "c", "d", "e", "f", "g", "h"]
     delegates = ["L", "R", "K"]
     moves = []
     while len(delegates) > 0:
         del_moves = []
-        del_pieces = request.post(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random(len(delegates)))).text.split(",")
+        del_pieces = request.post(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random.randrange(len(delegates)))).text.split(",")
         for piece in del_pieces:
             del_moves.append(request.post(url = URL + "actions-for?coord=" + boardCord[piece[0]]+str(piece[1]+1)).text.split("\n"))
-        random_move = del_moves[random(del_moves)]
+        random_move = del_moves[random.randrange(len(del_moves))]
         submitMove(request.post(url = URL + "actions-for?coord=" + move)) ##this would be where the ai would send to the move to rules and does everything with the board state
         moves.append(random_move)
     return moves
