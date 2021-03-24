aad5f2d74b2db4bc416dda7da1fa6e233b2fca05
Haley <arizonagranger@gmail.com>
2021-03-24 14:45:58 -0400

    fixed requests call but typeerror occurred

--- AI/randomAI.py
+++ AI/randomAI.py
@@ -7,18 +7,20 @@ def random_move(team):
     moves = []
     while len(delegates) > 0:
         del_moves = []
-        del_pieces = requests.post(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random.randrange(len(delegates)))).text.split(",")
+        del_pieces = requests.get(url = URL + "pieces?team=" + team + "&corp=" + delegates.pop(random.randrange(len(delegates)))).text.split(",")
         for piece in del_pieces:
-            del_moves.append(requests.post(url = URL + "actions-for?coord=" + piece).text.split("\n"))
+            del_moves.append(requests.get(url = URL + "actions-for?coord=" + piece).text.split("\n"))
         random_move = del_moves[random.randrange(len(del_moves))]
-        requests.post(url = URL + "actions-for?coord=" + random_move) ##this would be where the ai would send to the move to rules and does everything with the board state
+        actions = requests.get(url = URL + "actions-for?coord=" + random_move) ##this would be where the ai would send to the move to rules and does everything with the board state
         moves.append(random_move)
     return moves
 
 
+moves = random_move("Black")
 
+for i in moves:
+    print(i)
 
-print(random_move("Black"))
 
 
 
