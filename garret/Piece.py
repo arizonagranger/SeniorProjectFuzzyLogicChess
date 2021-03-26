ee1e6898e9c1e56ff293a098c560e2d419d36d63
gdepew11 <61752838+gdepew11@users.noreply.github.com>
2021-03-26 15:36:50 -0400

    Add files via upload

--- garret/Piece.py
+++ garret/Piece.py
@@ -1,10 +1,6 @@
 class Piece:
-    def __init__(self, team, unit, delegation, coord):
-        speeds = {"i": 1, "a": 1, "n": 5, "p": 1, "q": 3, "k": 3}
-        self.speed = speeds[unit]
+
+    def __init__(self, team, type, delegation):
         self.team = team
-        self.unit = unit
+        self.type = type
         self.delegation = delegation
-        self.coord = coord
-        self.attack = True
-        self.move = True
