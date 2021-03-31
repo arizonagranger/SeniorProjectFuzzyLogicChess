f5242829dd213c7ed8a1d31429bcf626bf47ee6d
gdepew11 <61752838+gdepew11@users.noreply.github.com>
2021-03-31 14:56:08 -0400

    Update GUI.py

--- Garret/GUI.py
+++ Garret/GUI.py
@@ -1,37 +1,89 @@
-import pygame
+import pygame as p
+from Board import Board
+from RandomAI import RandomAI
+
+p.init()
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def load_images():
+    units = ["a","i","p","q","k","n"]
+    corps = ["L","K","R"]
+    for unit in units:
+        for corp in corps:
+            for team in range(2):
+                IMAGES[str(team)+unit+corp] = p.transform.scale(p.image.load("images/"+str(team)+unit+corp+".png"), (SQ_SIZE, SQ_SIZE))
 
 
-pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    p.display.set_caption("chess")
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    board = Board("WAKNRPRQKKKPLNLAKIRIRIRIKIKILILIL32iLiLiLiKiKiRiRiRaKnLpLqKkKpRnRaK")
+    a = RandomAI(board, 1)
+    load_images()
+    game_exit = False
+    piece_selected = []
+    while not game_exit:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                game_exit = True
+            elif event.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[1]//SQ_SIZE
+                row = location[0]//SQ_SIZE
+                if len(piece_selected) == 0 and board.board[col][row] is not None:
+                    piece_selected = [col, row]
+                elif len(piece_selected) != 0 and event.button == 1:
+                    board.move(piece_selected, [col, row])
+                    piece_selected = []
+                elif len(piece_selected) != 0 and event.button == 3:
+                    print("human: ", board.attack(piece_selected, [col, row]))
+                    piece_selected = []
+                elif len(piece_selected) != 0 and event.button == 2:
+                    board.del_piece(piece_selected, board.board[col][row].delegation)
+                    piece_selected = []
+            elif event.type == p.KEYDOWN:
+                board.end_turn()
+        if len(piece_selected) == 0:
+            p.display.set_caption("select a piece")
+        else:
+            p.display.set_caption(str(piece_selected)+": selected")
+        if board.turn == 1:
+            a.ai_move()
+        draw_game(screen, board)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+    p.quit()
+    quit()
 
-pygame.display.set_caption("Fuzzy Chess")
 
-size = 50
-white = (255,255,255)
-black = (0,0,0)
+def draw_game(screen, board):
+    draw_squares(screen)
+    draw_pieces(screen, board)
 
-running = True
-while running:
-    screen.fill((255, 255, 255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    boardLength = 8
-    screen.fill(white)
+def draw_squares(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            color = colors[((x+y)%2)]
+            p.draw.rect(screen, color, p.Rect(y*SQ_SIZE, x*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-    cnt = 0
-    for i in range(1, boardLength + 1):
-        for z in range(1, boardLength + 1):
-            # check if current loop value is even
-            if cnt % 2 == 0:
-                pygame.draw.rect(screen, white, [size * z, size * i, size, size])
-            else:
-                pygame.draw.rect(screen, black, [size * z, size * i, size, size])
-            cnt += 1
-        # since theres an even number of squares go back one value
-        cnt -= 1
-    # Add a nice boarder
-    pygame.draw.rect(screen, black, [size, size, boardLength * size, boardLength * size], 1)
-    pygame.display.update()
+
+def draw_pieces(screen, board):
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            piece = board.board[x][y]
+            if piece is not None:
+                screen.blit(IMAGES[str(piece.team) + piece.unit + piece.delegation], p.Rect(y*SQ_SIZE, x*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == '__main__':
+    main()
