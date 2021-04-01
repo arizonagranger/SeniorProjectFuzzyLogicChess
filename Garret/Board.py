58982dfa6a047366efeae3227dc7dd9707b53700
gdepew11 <61752838+gdepew11@users.noreply.github.com>
2021-04-01 01:04:32 -0400

    Update Board.py

--- Garret/Board.py
+++ Garret/Board.py
@@ -56,6 +56,7 @@ class Board:
                     x += 1
         self.set_turn(self.turn)
 
+    #sets all the pieces on the team that cant move
     def set_turn(self, team):
         for x in self.board:
             for y in x:
@@ -63,6 +64,7 @@ class Board:
                     y.attack = False
                     y.move = False
 
+    #returns the board string
     def get_board(self):
         if self.turn == 0:
             boardString = "W"
@@ -82,7 +84,8 @@ class Board:
                 else:
                     number += 1
         return boardString
-
+    
+    #returns all coordinates of a delegation in a list
     def get_del(self, team, delegation):
         self.update_pieces()
         del_list = []
@@ -91,7 +94,8 @@ class Board:
                 if y is not None and y.delegation == delegation and y.team == team:
                     del_list.append(y.coord)
         return del_list
-
+    
+    #returns a list of the coordinates of all pieces
     def get_team(self, team):
         del_list = []
         for x in self.board:
@@ -99,7 +103,8 @@ class Board:
                 if y is not None and y.team == team:
                     del_list.append(y.coord)
         return del_list
-
+    
+    #returns a list of all possible locations to move for the piece
     def get_moves(self, coord):
         self.update_pieces()
         if self.board[coord[0]][coord[1]].move:
@@ -123,7 +128,8 @@ class Board:
             return res
         else:
             return []
-
+    
+    #called by get move and is recursivley called to find the reach of a piece
     def get_moves_speed(self, coord, speed):
         moves = []
         if coord[0] >= 8 or coord[0] < 0 or coord[1] >= 8 or coord[1] < 0 or self.board[coord[0]][coord[1]] != None:
@@ -137,6 +143,7 @@ class Board:
                     moves += (self.get_moves_speed([coord[0] + y, coord[1] + x], speed - 1))
         return moves
 
+    #returns a list of the possible places to attck for a piece
     def get_attacks(self, coord):
         self.update_pieces()
         if self.board[coord[0]][coord[1]].attack:
@@ -179,6 +186,7 @@ class Board:
         else:
             return []
 
+    #activates a move returns true if the move happened
     def move(self, piece, coord):
         self.update_pieces()
         if self.board[piece[0]][piece[1]] is not None and coord in self.get_moves(piece) and self.board[piece[0]][
@@ -194,13 +202,15 @@ class Board:
             return True
         else:
             return False
-
+    
+    #sets all the pieces coordinates to where they are on the field
     def update_pieces(self):
         for x in range(len(self.board)):
             for y in range(len(self.board)):
                 if self.board[x][y] is not None:
                     self.board[x][y].coord = [x, y]
 
+    #returns true or false if the coord is in the range of the attacking unit at the attacking location
     def in_range(self, unit, attacker, coord):
         for x in [0, -1, 1]:
             for y in [-1, 1]:
@@ -226,11 +236,15 @@ class Board:
                                 pass
         return False
 
+    #sets the attack and returns the outcome and role in a list 0 if failed and 1 if succsess
+    #[1 or 0, 1-6 (roll)]
     def attack(self, attacker, defender):
         self.update_pieces()
         if self.board[attacker[0]][attacker[1]] is not None and defender in self.get_attacks(attacker):
             self.update_pieces()
             roll = random.randrange(1, 7)
+            self.board[attacker[0]][attacker[1]].move = False
+            self.board[attacker[0]][attacker[1]].attack = False
             if self.board[attacker[0]][attacker[1]].unit == "n" and not self.board[attacker[0]][attacker[1]].move:
                 roll -= 1
             if roll >= self.attack_values[
@@ -266,17 +280,21 @@ class Board:
         else:
             return [0, 0]
 
+    #activates if the corp leader is taken then sets all pieces in that corp to the kings corp
     def corp_dead(self, team, corps):
         for pieces in self.get_del(team, corps):
             self.board[pieces[0]][pieces[1]].delegation = "K"
-
+    
+    #sets piece to the given corp if its legal
     def del_piece(self, piece, corps):
         if self.board[piece[0]][piece[1]].delegation == "K" and self.board[piece[0]][piece[1]].unit != "k":
             self.board[piece[0]][piece[1]].delegation = corps
 
+    #activates if king is dead
     def king_dead(self, team):
         self.state = abs(team - 1)
 
+    #switches the board to the next teams turn 
     def end_turn(self):
         for x in self.board:
             for y in x:
@@ -286,22 +304,27 @@ class Board:
         self.turn = abs(self.turn - 1)
         self.set_turn(self.turn)
 
+    #returns the piece at the given coord
     def get_piece(self, coord):
         return self.board[coord[0]][coord[1]]
 
+    #adds a piece to the board
     def add_piece(self, team, unit, delegation, coord):
         self.board[coord[0]][coord[1]] = Piece(team, unit, delegation, coord)
         self.update_pieces()
 
+    #removes piece from given coord
     def delete_piece(self, coord):
         self.board[coord[0]][coord[1]] = None
         self.update_pieces()
 
+    #moves piece to the given coord
     def set_piece(self, piece, coord):
         self.board[coord[0]][coord[1]] = self.board[piece[0]][piece[1]]
         self.board[piece[0]][piece[1]] = None
         self.update_pieces()
 
+    #prints a board
     def show_board(self):
         for x in range(len(self.board)):
             print()
@@ -315,7 +338,9 @@ class Board:
                     print("0", end=" ")
         print("\n")
 
+    #prints board showing possible moves
     def show_moves(self, coord):
+        moves = self.get_moves(coord)
         if self.board[coord[0]][coord[1]] is not None:
             for x in range(8):
                 for y in range(8):
@@ -326,12 +351,13 @@ class Board:
                             print(self.board[x][y].unit.upper(), end=" ")
                         else:
                             print(self.board[x][y].unit, end=" ")
-                    elif [x, y] in self.get_moves(coord):
+                    elif [x, y] in moves:
                         print(u"\u25A0", end=" ")
                     else:
                         print(0, end=" ")
                 print()
 
+    #prints board with higlighted enemies to attack
     def show_attacks(self, coord):
         attacks = self.get_attacks(coord)
         print(attacks)
