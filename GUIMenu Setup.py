d38e468d55158e21785e95b63e447b6d05e50a05
kiwibarry <49766351+LoveDiscord@users.noreply.github.com>
2021-05-03 10:06:53 -0400

    Updated the remainder, still needs rules info and fixing of the errors

--- GUIMenu Setup.py
+++ GUIMenu Setup.py
@@ -3,9 +3,21 @@
 
 import pygame as p
 
-#Menu function: what we're gonna use to navigate the games, rules, and credits
+from Garret.GUI import Button
+from Garret.GUI import HEIGHT
+from Garret.GUI import SCREEN
+from Garret.GUI import WIDTH
+from Garret.GUI import load_images
+from Garret.GUI import start_game
+from Garret.GUI import text_objects
+
+
+def main():
+    mainMenu()
+
+# Menu function: what we're gonna use to navigate the games, rules, and credits
 def mainMenu():
-    loadImages()
+    load_images()
     menu = True
 
     while menu:
@@ -13,29 +25,29 @@ def mainMenu():
             p.quit()
             quit()
 
-        screen.fill(p.Color("white"))
+        SCREEN.fill(p.Color("white"))
 
-        #Images needed to be loaded in for the main menu
+        # Images needed to be loaded in for the main menu
         # Im1 = p.image.load
-        #position the images on the screen
-        #screen.blit(i1
+        # position the images on the screen
+        # screen.blit(i1
 
-        #Text for the main menu
-        largeText = p.font.SysFont("Calibri")
-        TextSurf, TextRect = text_obj("4D Chess AI", largeText)
+        # Text for the main menu
+        largeText = p.font.SysFont("Calibri", 30)
+        TextSurf, TextRect = text_objects("Medieval Fuzzy Logic Chess 4C", largeText)
         TextRect.center = (int(WIDTH /2), int(HEIGHT /2+30))
-        screen.blit(TextSurf, TextRect)
+        SCREEN.blit(TextSurf, TextRect)
 
-        #Buttons to use
-        button("Play", new_game) #Needs positioning and color of the button
-        button("Credits", ) #Positioning, color, we also need a credit screen
-        button("Rules", ) #Positioning, color, rules screen
+        # Buttons to use
+        Button("Play", int(WIDTH/2 -250), 450, 100, 50, p.Color(180, 180, 180), p.Color(100, 100, 100), start_game())
+        Button("Credits", int(WIDTH/2 -50), 450, 100, 50, p.Color(180, 180, 180), p.Color(100, 100, 100), creditScreen()) # Positioning, color, we also need a credit screen
+        Button("Rules", int(WIDTH/2 -250), 450, 100, 50, p.Color(180, 180, 180), p.Color(100, 100, 100), ruleScreen()) # Positioning, color, rules screen
 
-        clock.tick(MAX_FPS)
+        # clock.tick(MAX_FPS)
         p.display.flip()
 
 
-#Displays what the rules engine is setup to do in text
+# Displays what the rules engine is setup to do in text
 def ruleScreen():
     rules = True
 
@@ -45,17 +57,18 @@ def ruleScreen():
                 p.quit()
                 quit()
 
-    screen.fill(p.Color("white"))
-    largeText = p.font.SysFont("Calibri")
+    SCREEN.fill(p.Color("white"))
+    largeText = p.font.SysFont("Calibri", 30)
+    smallText = p.font.SysFont("Calibri", 14)
 
-    TextSurf, TextRect = text_obj("Rules", largeText)
-    TextRect.center = () #positioning
+    TextSurf, TextRect = text_objects("Rules", largeText)
+    TextRect.center = (int(WIDTH / 2), int(HEIGHT / 2 + 30))
 
 
     p.display.flip()
 
 
-#Displays everyone involved and if we want to we can add our pictures
+# Displays everyone involved and if we want to we can add our pictures
 def creditScreen():
     credit = True
 
@@ -66,4 +79,29 @@ def creditScreen():
                 quit()
 
 
-    screen.fill(p.Color("white"))
+    SCREEN.fill(p.Color("white"))
+    largeText = p.font.SysFont("Calibri", 30)
+    nameText = p.font.SysFont("Calibri", 14)
+
+    TextSurf, TextRect = text_objects("Credits", largeText)
+    TextRect.center = (int(WIDTH / 2), int(HEIGHT / 2 + 30))
+
+    TextSurf, TextRect = text_objects("Haley Granger", nameText)
+    TextRect.center = (int(WIDTH / 2), 150)
+    SCREEN.blit(TextSurf, TextRect)
+
+    TextSurf, TextRect = text_objects("Garret Depew", nameText)
+    TextRect.center = (int(WIDTH / 2), 170)
+    SCREEN.blit(TextSurf, TextRect)
+
+    TextSurf, TextRect = text_objects("Alexandar Eubanks", nameText)
+    TextRect.center = (int(WIDTH / 2), 200)
+    SCREEN.blit(TextSurf, TextRect)
+
+    TextSurf, TextRect = text_objects("Jacob Walton", nameText)
+    TextRect.center = (int(WIDTH / 2), 250)
+    SCREEN.blit(TextSurf, TextRect)
+
+    TextSurf, TextRect = text_objects("Kiah Jefferies", nameText)
+    TextRect.center = (int(WIDTH / 2), 270)
+    SCREEN.blit(TextSurf, TextRect)
