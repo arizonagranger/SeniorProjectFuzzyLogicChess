6c85f8c8021d21eb47a68e3a023638e0a6568fda
kiwibarry <49766351+LoveDiscord@users.noreply.github.com>
2021-05-04 15:15:01 -0400

    Finished Rules, about to add over to GUI to see if error goes away

--- GUIMenu Setup.py
+++ GUIMenu Setup.py
@@ -10,6 +10,7 @@ from Garret.GUI import WIDTH
 from Garret.GUI import load_images
 from Garret.GUI import start_game
 from Garret.GUI import text_objects
+from Garret.Board import Board
 
 
 def main():
@@ -59,11 +60,58 @@ def ruleScreen():
 
     SCREEN.fill(p.Color("white"))
     largeText = p.font.SysFont("Calibri", 30)
-    smallText = p.font.SysFont("Calibri", 14)
+    meduiumText = p.font.SysFont("Calibri", 15)
+    smallText = p.font.SysFont("Calibri", 12)
 
     TextSurf, TextRect = text_objects("Rules", largeText)
     TextRect.center = (int(WIDTH / 2), int(HEIGHT / 2 + 30))
 
+    TextSurf, TextRect = text_objects("The armies of chess pieces for command are divided into three corp:",
+                                      meduiumText)
+    TextRect.center = (int(WIDTH / 2), 100)
+
+    TextSurf, TextRect = text_objects("The King and Bishops are corp commanders, each with one command "
+                                      "authority that can be used in each turn.", smallText)
+    TextRect.center = (int(WIDTH / 2), 115)
+
+    TextSurf, TextRect = text_objects("Up to three actions may be taken in each turn, one by each corp. "
+                                      "Corp command actions ARE NOT required in any turn, so zero to "
+                                      "three actions may be taken in a turn.", smallText)
+    TextRect.center = (int(WIDTH / 2), 130)
+
+    TextSurf, TextRect = text_objects("Command authority may not be transferred from one commander to another.",
+                                      smallText)
+    TextRect.center = (int(WIDTH / 2), 150)
+
+    TextSurf, TextRect = text_objects("The left side Bishop commands the three left pawns and the left-side Knight.",
+                                      smallText)
+    TextRect.center = (int(WIDTH / 2), 170)
+
+    TextSurf, TextRect = text_objects("The right side Bishop commands the three right pawns and the right-side Knight.",
+                                      smallText)
+    TextRect.center = (int(WIDTH / 2), 190)
+
+    TextSurf, TextRect = text_objects("The King commands the Queen, the two rooks (archers), and the "
+                                      "remaining two center pawns", smallText)
+    TextRect.center = (int(WIDTH / 2), 210)
+
+    TextSurf, TextRect = text_objects("The King may delegate any of its pieces to be commanded by either "
+                                      "Bishop, at any time.", smallText)
+    TextRect.center = (int(WIDTH / 2), 230)
+
+    TextSurf, TextRect = text_objects("Each commander (King and Bishops) may command one action (move or capture) "
+                                      "per turn,only for their commanded pieces.", smallText)
+    TextRect.center = (int(WIDTH / 2), 250)
+
+    TextSurf, TextRect = text_objects("When a Bishop is captured, "
+                                      "his commanded pieces revert to the command of the King, but his command "
+                                      "authority is lost (the army may make one fewer action per turn, "
+                                      "a serious disadvantage)", smallText)
+    TextRect.center = (int(WIDTH / 2), 270)
+
+    TextSurf, TextRect = text_objects("The game ends when the King is captured "
+                                      "(the Bishops do not fight on without the King, but surrender)", smallText)
+    TextRect.center = (int(WIDTH / 2), 290)
 
     p.display.flip()
 
@@ -87,21 +135,21 @@ def creditScreen():
     TextRect.center = (int(WIDTH / 2), int(HEIGHT / 2 + 30))
 
     TextSurf, TextRect = text_objects("Haley Granger", nameText)
-    TextRect.center = (int(WIDTH / 2), 150)
+    TextRect.center = (int(WIDTH / 2), 100)
     SCREEN.blit(TextSurf, TextRect)
 
     TextSurf, TextRect = text_objects("Garret Depew", nameText)
-    TextRect.center = (int(WIDTH / 2), 170)
+    TextRect.center = (int(WIDTH / 2), 130)
     SCREEN.blit(TextSurf, TextRect)
 
     TextSurf, TextRect = text_objects("Alexandar Eubanks", nameText)
-    TextRect.center = (int(WIDTH / 2), 200)
+    TextRect.center = (int(WIDTH / 2), 170)
     SCREEN.blit(TextSurf, TextRect)
 
     TextSurf, TextRect = text_objects("Jacob Walton", nameText)
-    TextRect.center = (int(WIDTH / 2), 250)
+    TextRect.center = (int(WIDTH / 2), 210)
     SCREEN.blit(TextSurf, TextRect)
 
     TextSurf, TextRect = text_objects("Kiah Jefferies", nameText)
-    TextRect.center = (int(WIDTH / 2), 270)
+    TextRect.center = (int(WIDTH / 2), 240)
     SCREEN.blit(TextSurf, TextRect)
